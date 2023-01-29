5e89dd4fe8e74d270f93db1c107cc4f189b74ce7
Kei Nakayama <kei.of.nakayama@gmail.com>
2023-01-29 22:05:23 +0900

    fix

--- packages/cdk/lib/cdk-stack.ts
+++ packages/cdk/lib/cdk-stack.ts
@@ -1,19 +1,27 @@
-import { Duration, Stack, StackProps } from 'aws-cdk-lib';
-import * as sns from 'aws-cdk-lib/aws-sns';
-import * as subs from 'aws-cdk-lib/aws-sns-subscriptions';
-import * as sqs from 'aws-cdk-lib/aws-sqs';
-import { Construct } from 'constructs';
+import { Construct } from 'constructs'
+import { Stack, StackProps, aws_lambda, aws_apigateway } from 'aws-cdk-lib'
 
-export class CdkStack extends Stack {
+export class IacStack extends Stack {
   constructor(scope: Construct, id: string, props?: StackProps) {
-    super(scope, id, props);
+    super(scope, id, props)
 
-    const queue = new sqs.Queue(this, 'CdkQueue', {
-      visibilityTimeout: Duration.seconds(300)
-    });
-
-    const topic = new sns.Topic(this, 'CdkTopic');
-
-    topic.addSubscription(new subs.SqsSubscription(queue));
+    const fn = new aws_lambda.Function(this, 'fn', {
+      code: aws_lambda.Code.fromAsset('../server/dist'),
+      handler: 'index.handler',
+      runtime: aws_lambda.Runtime.NODEJS_14_X,
+      environment: {
+        NODE_OPTIONS: '--enable-source-maps',
+      },
+    })
+    const api = new aws_apigateway.RestApi(this, 'api', {
+      defaultCorsPreflightOptions: {
+        allowOrigins: aws_apigateway.Cors.ALL_ORIGINS,
+        allowMethods: aws_apigateway.Cors.ALL_METHODS,
+        allowHeaders: aws_apigateway.Cors.DEFAULT_HEADERS,
+      },
+    })
+    api.root
+        .addResource('{proxy+}')
+        .addMethod('ANY', new aws_apigateway.LambdaIntegration(fn))
   }
 }
